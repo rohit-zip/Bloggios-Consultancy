78442efcefb71b625d7f8e1ad875ef7b2e6d8c4a
Rohit Parihar <rohitparih@gmail.com>
2023-06-09 23:01:04 +0530

    Bottom-Bar

--- src/Screen/Landing.js
+++ src/Screen/Landing.js
@@ -1,9 +1,25 @@
+import Lottie from 'lottie-react'
 import React from 'react'
-import "../Style/Landing.css"
+import { useRef } from 'react';
+import bloggios_logo from '../Asset/Lottie/a2.json'
+import { useEffect } from 'react';
+import '../Style/Landing.css'
+import BottomBar from '../Component/Bottom-Bar/BottomBar';
 
 const Landing = () => {
+    // const isPlay = true;
+    // const lottieRef = useRef();
+    // useEffect(() => {
+    //     isPlay ? lottieRef.current.play() : lottieRef.current.pause();
+    // })
+    // return (
+    //     <Lottie lottieRef={lottieRef} animationData={bloggios_logo} />
+    // )
+
     return (
-        <div className='main-div'>Landing</div>
+        <div className='landing-div' >
+            <BottomBar />
+        </div>
     )
 }
 
