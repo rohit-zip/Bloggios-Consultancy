5a3026e322fd881106e7eb93bf2f780f9f952191
Rohit Parihar <rohitparih@gmail.com>
2023-06-09 13:05:27 +0530

    Structure Initialization

---
+++ src/Screen/Landing.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "../Style/Landing.css"
+
+const Landing = () => {
+    return (
+        <div className='main-div'>Landing</div>
+    )
+}
+
+export default Landing
--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, RouterProvider, Routes, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
+import Landing from './Screen/Landing';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
