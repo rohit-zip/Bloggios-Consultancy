822b92bc9483ea68979db6fe6e9ee1ed99cb9855
Rohit Parihar <rohitparih@gmail.com>
2023-06-14 16:34:33 +0530

    Home Screen Top Section Development Part-1

--- src/Screen/HomeScreen.js
+++ src/Screen/HomeScreen.js
@@ -1,17 +1,11 @@
 import React from 'react'
 import Landing from '../Component/Landing/Landing'
+import HomeScreenTopSection from '../Component/HomeScreen/HomeScreenTopSection'
 
 const HomeScreen = () => {
     return (
         <Landing>
-            <div className='bg-body row mx-5 my-4'>
-                <div className="col-lg-4 col-md-12 col-sm-12 bg-success">
-                    Rohit
-                </div>
-                <div className="col-lg-8 col-md-12 col-sm-12  bg-info">
-                    Rohit
-                </div>
-            </div>
+            <HomeScreenTopSection />
         </Landing>
     )
 }
