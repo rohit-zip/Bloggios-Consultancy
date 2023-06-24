1a4f8aeab671dce1954e18e7be8d993f23273268
Rohit Parihar <rohitparih@gmail.com>
2023-06-24 22:37:45 +0530

    Major Changes

---
+++ src/Base/Base.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from '../Navbar/Navbar'
+import './Base.css'
+
+const Base = ({ children }) => {
+    return (
+            <div className='homescreen-backdrop'>
+                <Navbar />
+                <div>
+                    {children}
+                </div>
+            </div>
+    )
+}
+
+export default Base
