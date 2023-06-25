902453932c2370eed5a9b1bc5f3eb9d631a9cbfb
Rohit Parihar <rohitparih@gmail.com>
2023-06-25 14:30:33 +0530

    Minor Changes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: "/Bloggios-Consultancy",
     element: <HomeScreen />,
     // children: [
     //   {
