a958648e96a10cf54ecc308f416a7cf77e47bf30
Rohit Parihar <rohitparih@gmail.com>
2023-06-26 22:38:24 +0530

    Best Section Implementation Part 1

---
+++ src/HomeScreen/Components/BestChoice.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import '../Styles/BestChoice.css'
+
+const BestChoice = () => {
+    return (
+        <section className='best-choice'>
+            <div>
+                <div>
+                    <span className='best-choice-header'>Why Bloggios is best choice<br />for your unique idea</span>
+                    <br />
+                    <span className='best-choice-motto'>Bloggios is affordable and reliable Software Development Company that can help you create strong presence in this Modern World</span>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default BestChoice
