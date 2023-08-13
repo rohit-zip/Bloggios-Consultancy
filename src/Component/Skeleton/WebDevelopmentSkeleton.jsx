0c12b83b7599056a574a89a3b6bb54798b182b69
Rohit Parihar <rohitparih@gmail.com>
2023-08-13 16:56:18 +0530

    List Servoces Added Part 1

---
+++ src/Component/Skeleton/WebDevelopmentSkeleton.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Styles/WebDevelopmentSkeleton.css'
+
+const WebDevelopmentSkeleton = () => {
+    return (
+        <div className='background-banner'>
+            <div className='display-banner'>
+                <div className='screen-header'>
+                    <div className='status-buttons'>
+                        <div />
+                        <div />
+                        <div />
+                    </div>
+                    <div className='status-bar-grey-pane' />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default WebDevelopmentSkeleton
---
+++ src/HomeScreen/Components/OurServices.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import '../Styles/OurServices.css'
+import ListRow from './ListRow'
+import mobile_bloggios from '../../Assets/SVG/mobile-bloggios.svg'
+import web_bloggios from '../../Assets/SVG/web-design.svg'
+
+const listData = [
+    {
+        title: "Web Development",
+        image: web_bloggios,
+        alt: 'web-development',
+        body: "Websites and E-Commerce do not have to be expensive or complicated to be effective. We deal with the wide spectrum of clients with varying budgets and needs."
+    },
+    {
+        title: "App Development",
+        image: mobile_bloggios,
+        alt: 'app-development',
+        body: "Websites and E-Commerce do not have to be expensive or complicated to be effective. We deal with the wide spectrum of clients with varying budgets and needs."
+    }
+]
+
+const OurServices = () => {
+    return (
+        <section className='our-services'>
+            <div className='our-services-container'>
+                <span>We help Buisnesses to make their Product come to life, worldwide.</span>
+                {listData.map((value, key) => {
+                    return (
+                        <ListRow
+                            image={value.image}
+                            title={value.title}
+                            alt={value.alt}
+                            body={value.body} />
+                    )
+                })}
+            </div>
+        </section>
+    )
+}
+
+export default OurServices
