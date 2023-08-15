a0995d860e70c6cc0a9676d3f10e170458b409cb
Rohit Parihar <rohitparih@gmail.com>
2023-08-15 16:20:59 +0530

    Our Services Mobile Responsiveness Added

--- src/HomeScreen/Components/ListRow.jsx
+++ src/HomeScreen/Components/ListRow.jsx
@@ -1,21 +1,20 @@
 import React from 'react'
-import web_design from '../../Assets/SVG/web-designsvg.svg'
 
 
 const ListRow = (props) => {
   return (
     <div className='list-row'>
-    <div>
+      <div>
         <img className='images' src={props.image} alt={props.alt} />
-    </div>
-    <div className='right-pane-services'>
+      </div>
+      <div className='right-pane-services'>
         <div>
-            <div>{props.title}</div>
-            <span>{props.body}</span>
+          <div>{props.title}</div>
+          <span>{props.body}</span>
         </div>
         <button>Learn more</button>
+      </div>
     </div>
-</div>
   )
 }
 
