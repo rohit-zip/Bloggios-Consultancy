2b6718e9d0a00341c2c18d1d2ab13ff4f195e414
Rohit Parihar <rohitparih@gmail.com>
2023-08-16 14:21:29 +0530

    Footer Added

--- src/HomeScreen/Components/Footer.jsx
+++ src/HomeScreen/Components/Footer.jsx
@@ -1,11 +1,61 @@
 import React from 'react'
-import useWindowDimensions from '../../Services/WindowsDimesions'
+import '../Styles/Footer.css'
 
 const Footer = () => {
 
-    const { width, height } = useWindowDimensions();
     return (
-        <div style={{ height: '60vh', fontSize: '40px' }}>{height}</div>
+        <section className='bloggios-landing-footer'>
+            <div className='landing-footer-div d-flex'>
+                <div className="col-5">
+                    <span>Find Us</span>
+                    <div className='footer-icons-div'>
+                        <i class="fa-brands fa-instagram"></i>
+                        <i class="fa-brands fa-facebook"></i>
+                        <i class="fa-brands fa-square-twitter"></i>
+                        <i class="fa-brands fa-linkedin"></i>
+                    </div>
+                </div>
+                <div className="col-7 d-flex footer-services-section">
+                    <div className="col">
+                        <span>Dev Service</span>
+                        <div>
+                            <span>Web Development</span>
+                            <span>App Development</span>
+                            <span>Backend</span>
+                            <span>Frontend</span>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <span>Resources</span>
+                        <div>
+                            <span>Pricing</span>
+                            <span>Blog</span>
+                            <span>Projects</span>
+                            <span>About</span>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <span>Learn</span>
+                        <div>
+                            <span>Instructor</span>
+                            <span>Learner</span>
+                            <span>Courses</span>
+                            <span>Free Resource</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className='footer-last-section'>
+                <div>
+                    <span>Bloggios</span>
+                    <span>Term of Use</span>
+                    <span>Privacy Policy</span>
+                </div>
+                <div>
+                    <span>Copyright © 2023 Bloggios. All Right Reserved</span>
+                </div>
+            </div>
+        </section>
     )
 }
 
