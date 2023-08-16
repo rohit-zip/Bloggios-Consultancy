b27a209cc98f13297831136e866ba085652543b1
Rohit Parihar <rohitparih@gmail.com>
2023-08-16 12:30:26 +0530

    Booking Section Bug Fix

--- src/HomeScreen/Components/Footer.jsx
+++ src/HomeScreen/Components/Footer.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import useWindowDimensions from '../../Services/WindowsDimesions'
 
 const Footer = () => {
+
+    const { width, height } = useWindowDimensions();
     return (
-        <div style={{ height: '60vh' }}>Footer</div>
+        <div style={{ height: '60vh', fontSize: '40px' }}>{height}</div>
     )
 }
 
