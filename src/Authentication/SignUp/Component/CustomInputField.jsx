490db10e603888fe8e72e21f330573623643ecfc
Rohit Parihar <rohitparih@gmail.com>
2023-08-20 17:33:21 +0530

    Password Validation

--- src/Authentication/SignUp/Component/CustomInputField.jsx
+++ src/Authentication/SignUp/Component/CustomInputField.jsx
@@ -1,15 +1,35 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import '../Styles/CustomInputField.css'
 
 const CustomInputField = (props) => {
+
+    const [isVisible, setIsVisible] = useState(false);
+    const [isFocus, setIsFocus] = useState(false);
+    const textInputRef = useRef(null);
+
+    const handleClick = () => {
+        setIsFocus(true);
+    }
+
     return (
         <div className='input-field-custom'>
             <span>{props.label}</span>
-            <input 
-                type={props.type}
-                onChange={props.onChange}
-                value={props.value}
-                placeholder={props.placeholder} />
+            <div className='input-field-div' style={{
+                border: props.borderColor,
+                backgroundColor: props.backgroundColor
+            }}>
+                <input
+                    type={props.type}
+                    onChange={props.onChange}
+                    value={props.value}
+                    ref={textInputRef}
+                    onClick={() => handleClick()}
+                    onKeyUp={props.onKeyUp}
+                    placeholder={props.placeholder} />
+                {
+                    props.eyeIcon && <i onClick={props.passwordVisible} className={`fa-regular ${props.visibility ? 'fa-eye' : 'fa-eye-slash'}`}></i>
+                }
+            </div>
         </div>
     )
 }
--- src/Authentication/SignUp/Component/Signup.jsx
+++ src/Authentication/SignUp/Component/Signup.jsx
@@ -4,23 +4,13 @@ import coding_image from '../../../Assets/SVG/undraw_sharing_knowledge_03vp.svg'
 import bloggios_logo from '../../../Assets/SVG/bloggios-white-purple-logo.svg'
 import CustomInputField from './CustomInputField'
 import { signUp } from '../../../Services/RestServices/UserServiceApi'
+import { isVisible } from '@testing-library/user-event/dist/utils'
 
-const inputList = [
-    {
-        label: 'Email',
-        placeholder: 'Enter your Email',
-        type: 'text',
-        constraint: 'email'
-    },
-    {
-        label: 'Password',
-        placeholder: 'Enter your password',
-        type: 'password',
-        constraint: 'password'
-    }
-]
 
 const Signup = () => {
+
+    const [isVisible, setIsVisible] = useState(false);
+    const [isErrorPassword, setIsErrorPassword] = useState(false);
 
     const [data, setData] = useState(
         {
@@ -30,8 +20,8 @@ const Signup = () => {
     )
 
     useEffect(() => {
-        console.log(data);
-    }, [data])
+
+    }, [isErrorPassword])
 
     const handleChange = (event, property) => {
         setData({
@@ -52,6 +42,32 @@ const Signup = () => {
             }).catch((error) => console.log(error))
     }
 
+    const handleValidation = (event, property) => {
+        if (property === 'password') {
+            let passwordValue = event.target.value;
+            passwordValue.length < 8 || passwordValue.length > 20 ? setIsErrorPassword(true) : setIsErrorPassword(false)
+        }
+    }
+
+    const inputList = [
+        {
+            label: 'Email',
+            placeholder: 'Enter your Email',
+            type: 'text',
+            constraint: 'email',
+            borderColor: '1px solid rgba(255, 255, 255, 0.6)',
+            backgroundColor: 'transparent'
+        },
+        {
+            label: 'Password',
+            placeholder: 'Enter your password',
+            type: isVisible ? 'text' : 'password',
+            constraint: 'password',
+            borderColor: isErrorPassword ? '1px solid rgba(255, 0, 0, 0.6)' : '1px solid rgba(255, 255, 255, 0.6)',
+            backgroundColor: isErrorPassword ? 'rgba(255, 0, 0, 0.1)' : 'transparent'
+        }
+    ]
+
     return (
         <section className='auth-bloggios-signup'>
             <div className='auth-signup-main-div'>
@@ -70,6 +86,13 @@ const Signup = () => {
                                     key={key}
                                     label={value.label}
                                     type={value.type}
+                                    isError={isErrorPassword}
+                                    borderColor={value.borderColor}
+                                    eyeIcon={value.constraint === 'password'}
+                                    onKeyUp={(e) => handleValidation(e, value.constraint)}
+                                    visibility={isVisible}
+                                    backgroundColor={value.backgroundColor}
+                                    passwordVisible={() => setIsVisible(!isVisible)}
                                     onChange={(e) => handleChange(e, value.constraint)}
                                     value={value.constraint === 'email' ? data.email : data.password}
                                     placeholder={value.placeholder} />
